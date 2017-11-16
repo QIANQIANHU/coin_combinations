364aa886df20f615addb18b976ec4f6d666e78e8
Malgorzata Haniszewska and Qianqian Hu <malgorzata@>
2017-11-16 13:36:31 -0800

    fix problems with logic

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -2,8 +2,8 @@ class Coins
   def initialize(number)
     @quarters = number/25
     @dimes = (number%25)/10
-    @nickels = ((number%25) - 10)/5
-    @pennies = ((number%25) - 10)-5
+    @nickels = ((number%25)%10)/5
+    @pennies = ((number%25)%10)%5
   end
   def result_displayer()
     "It is equal to #{@quarters} quarters, #{@dimes} dimes, #{@nickels} nickels, #{@pennies} pennies."
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -4,8 +4,8 @@ require('pry')
 
 describe('coin_combinations') do
   it("lets a user enter an amount of cents and shows the smallest number of quarters, dimes, nickels, and pennies needed to make that change") do
-    new_coin = Coins.new(41)
-    expect(new_coin.result_displayer).to(eq("It is equal to 1 quarters, 1 dimes, 1 nickels, 1 pennies."))
+    new_coin = Coins.new(57)
+    expect(new_coin.result_displayer).to(eq("It is equal to 2 quarters, 0 dimes, 1 nickels, 2 pennies."))
   end
 
 end
